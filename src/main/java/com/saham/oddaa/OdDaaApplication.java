f8e51879a2278a13aefad37ef7efa21d96700ccd
0xAl3arab <mygamingali2004@gmail.com>
2026-09-05 15:13:25 +0100

    jenkins file

---
+++ src/main/java/com/saham/oddaa/OdDaaApplication.java
@@ -0,0 +1,13 @@
+package com.saham.oddaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdDaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdDaaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saham/oddaa/OdDaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saham.oddaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdDaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
